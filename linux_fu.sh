c234ed87ddf4001305e260fa1e664624e6a7a02d
legionJP <jjpsingh18@gmail.com>
2024-04-09 12:11:11 +0530

    Updating the command guide like cut , paste ..

--- linux_fu.sh
+++ linux_fu.sh
@@ -84,5 +84,118 @@ $ ls /fake/directory &> peanuts.txt
 ls /fake/directory 2> /dev/null
 
 #------------------
-#4. Pipe and Tree
+#4. pipe and tee  :
 # ----------------
+
+#pipe : The pipe operator |, represented by a vertical bar,
+ #allows us to get the stdout of a command and make that the stdin to another process. 
+
+ls -la \etc  
+#list of the  items but difficult ot read
+
+ls -la /etc | less
+
+# we took the stdout of ls -la /etc and then piped it to the less command. 
+# The pipe command is extremely useful and we will continue to use it for all eternity.
+
+#________
+# tee : if we wanted to write the output of our command to two different streams? That's possible with the tee command: 
+#--------
+
+ls | tee peanuts.txt
+
+ls | tee peanuts.txt banana.txt
+
+####################################################################
+
+#------------------
+# 5. env(Environment) :
+#-----------------
+#environment variables contain useful information that the shell and other processes can use.
+
+echo $HOME 
+#shows the path of the your directory: /home/jp
+
+echo $USER
+
+#all this commands are coming from our environment varriables
+
+env
+
+#Output : PATH=/home/jp/.local/bin:/usr/local/sbin:/usr/local/bin
+     #   PWD=/home/jp/Desktop/Linux_Repo
+     #  USER=jp
+
+#One particularly important variable is the PATH Variable.
+echo $PATH
+
+#------------
+# cut  :  In this their are lots of useful commands that you can use to process text including cut
+        #  cut extracts portions of text from a file.
+#--------------
+
+echo 'The quick brown; fox jumps over the lazy   dog' > sample.txt
+
+# extract the contents by character,  space also counts as a character.
+cut -c 5 sample.txt
+
+#extract the contents by a field,
+cut -f 2 sample.txt
+
+#The -f or field flag cuts text based off of fields, 
+#by default it uses TABs as delimiters, so everything separated by a TAB is considered a field. 
+
+#combine the field flag with the delimiter flag to extract the contents by a custom delimiter:
+
+cut -f 1 -d ";" sample.txt
+
+#This will change the TAB delimiter to a ";" delimiter , and result will be: first field
+
+cut -c 5-10 sample.txt
+
+cut -c 5- sample.txt
+
+cut -c -5 sample.txt
+
+
+#-------------------------------
+# paste : This is  similar to the cat command, it merges lines together in a file.
+#------------------------------------------------------------------------
+
+echo  
+'The
+
+quick
+
+brown
+
+fox' > sample2.txt
+
+#combine the all these lines into one line 
+paste -s sample2.txt
+
+# changing the delimiter (-d) of the TAB
+paste -d ' ' -s sample2.txt
+
+###########################################
+
+#--------------------------
+# head: 
+#-------------------
+
+# in  long file, in fact we have many to choose from, 
+# If we  ahead and cat /var/log/syslog. their is pages on pages 
+# so we can fix that with head command, by default the head command will show you the first 10 lines in a file.
+
+head /var/log/syslog
+
+#can also modify the line count to whatever you choose
+
+head -n 15 /var/log/syslog #line count
+
+head -c 15 /var/log/syslog # character count
+
+
+#------------------------------
+# tail : 
+#------------------------------
