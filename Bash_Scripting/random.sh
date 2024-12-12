9b6d591c6894bbb24ff5bbbb6bb4512167ed2d71
= <=>
2024-12-13 05:00:01 +0530

    add the random var

---
+++ Bash_Scripting/random.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for n in 1 2 3 4
+do
+    echo a New Random number is $RANDOM
+done
+
+echo Enter the word as argument
+read $name
+
+# check input length if the supplied or not
+[[ $# -eq 0 ]] && echo "Usage $0 word" && exit 1
+
+echo "$name-$RANDOM"
+exit 0
