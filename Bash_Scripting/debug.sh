ffec357bae4c34bc02b82d6e0ed17b3e5e2582f6
= <=>
2024-12-13 05:36:11 +0530

    update

--- Bash_Scripting/debug.sh
+++ Bash_Scripting/debug.sh
@@ -8,3 +8,8 @@ echo sum of the "$i" numbers is $sum
 ls error
 
 # redirect the error of this code execution by ./debug.sh 2> error.log 
+Question 17.6
+# If we want to debug the 15th line of a script, 
+# we must provide the _______ command on 14th line and the _______ command on 16th line of the script.
+
+# set -x and set +x
--- Bash_Scripting/random.sh
+++ Bash_Scripting/random.sh
@@ -9,7 +9,10 @@ echo Enter the word as argument
 read $name
 
 # check input length if the supplied or not
-[[ $# -eq 0 ]] && echo "Usage $0 word" && exit 1
+
+[[ $# -eq 0 ]] && echo "Usage: $0 word" && exit 1
 
 echo "$name-$RANDOM"
 exit 0
+
+#
