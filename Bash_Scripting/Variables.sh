ede9ab51e9e93523a57643fd040c6732c64cece7
legionJP <jjpsingh18@gmail.com>
2025-02-04 11:47:48 +0530

    varibales in bash rules and defining

---
+++ Bash_Scripting/Variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# sebang to specify the interpretter 
+
+NAME='JP'
+SPORT='Foot'
+
+# printing the var.
+echo "My name is $NAME"
+
+# concating the var data
+echo "The sport name is ${SPORT}ball"
+
+# variables defining  rule 
+STUDENT_01='John'
+# wrong
+#01_STUDENT='John'
